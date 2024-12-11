8d6ff1c8172ca066decd6342e1c7846ffbc5d625
LuwasFortune <obaikafortune@gmail.com>
2024-12-11 07:55:17 -0800

    data types (dynamic & booleans ) are added

--- Lesson 1/dataTypes.dart
+++ Lesson 1/dataTypes.dart
@@ -13,4 +13,12 @@ print (myAge);
 // Doubles
 double averageAge = 10.45;
 print (averageAge);
+
+// Dynamic
+dynamic dynamicNames = "Obaika Fortune is 41";
+print(dynamicNames);
+
+// Booleans
+bool statement = true;
+print(statement);
 }
